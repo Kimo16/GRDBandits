ae888836027b32eb2f8e8591efa3f8e8a18b1940
Paul <paultsk.pt@gmail.com>
2021-01-17 16:55:50 +0100

    vertices recovering and number of edges

--- Graph.java
+++ Graph.java
@@ -1,3 +1,6 @@
+import java.io.File;
+import java.util.Arrays;
+import java.util.Scanner;
 
 
 public class Graph
@@ -6,12 +9,53 @@ public class Graph
 
 	int nbEdges; 
 	int nbVertex;
-
+	int []from;
+	int []to;
 
 	int[][] adja;
 
-	public Graph()
+	public Graph(String fname, int estimNbAretes)
 	{
+		from = new int[estimNbAretes];
+		to = new int[estimNbAretes];
 
+		retrieveVertices(fname);
+		getNumEdge();
+
+	}
+
+	private void retrieveVertices(String fname) {
+		try {
+			int cpt =0;
+			File myObj = new File(fname);
+			Scanner myReader = new Scanner(myObj);
+			while (myReader.hasNextLine()) {
+				String[] data = myReader.nextLine().split("\t+", 2);
+
+				if( data[0].contains("#")) {
+					continue;
+				}
+				from[cpt] = Integer.valueOf(data[0]);
+				to[cpt++] = Integer.valueOf(data[1]);
+				
+			}
+			myReader.close();
+	    } catch (Exception e) {
+	    	System.out.println("Error");
+	    	e.printStackTrace();
+	    }
+	}
+
+	private long getNumEdge() {
+		
+		int[] all = new int[from.length + to.length];
+		System.arraycopy(to, 0, all, 0, from.length);
+		System.arraycopy(to, 0, all, from.length, to.length);
+		
+		System.out.println("all length: "+ all.length);
+		
+		long number = Arrays.stream(all).distinct().count();
+		System.out.println("distinct count : "+ number);
+		return number;
 	}
 }
--- Main.java
+++ Main.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Main
 {
 
@@ -8,5 +11,32 @@ public class Main
 		/*recupérer les informations*/
 		/*construire le graphe*/
 		/*effectuer les calculs demander*/
+
+		String fname = "as20000102.txt";
+		int nb = getLineNumber(fname);
+		Graph g = new Graph(fname, nb);
+	}
+
+	/* INUTILE pour le rendu vu que l'on a le nombre de ligne en parametre normalement*/
+	private static int getLineNumber(String fname) {
+		System.out.println("in function");
+		Process p;
+		String s;
+		int res = -1;
+		try {
+			// get the line count : wc -l
+			p = Runtime.getRuntime().exec("wc -l "+ fname);
+			BufferedReader br = new BufferedReader( new InputStreamReader(p.getInputStream()));
+			
+			/* single reading due to wc -l return type */
+			s = br.readLine();
+			res = Integer.valueOf(s.split(" ")[0]);
+			p.waitFor();
+			p.destroy();
+		}catch( Exception e) {
+			
+		}
+		System.out.println("end function");
+		return res;
 	}
 }
