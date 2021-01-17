47a8e94e8567575a236ea0a8e782705a8daebc7d
Karim Mezher <mezher@informatique.univ-paris-diderot.fr>
2021-01-17 17:13:13 +0100

    add build_adja

--- Graph.java
+++ Graph.java
@@ -9,6 +9,7 @@ public class Graph
 
 	int nbEdges; 
 	int nbVertex;
+
 	int []from;
 	int []to;
 
@@ -16,16 +17,19 @@ public class Graph
 
 	public Graph(String fname, int estimNbAretes)
 	{
+
 		from = new int[estimNbAretes];
 		to = new int[estimNbAretes];
 
 		retrieveVertices(fname);
 		getNumEdge();
-
+		build_adjacency_list();
 	}
 
-	private void retrieveVertices(String fname) {
-		try {
+	private void retrieveVertices(String fname) 
+	{
+		try 
+		{
 			int cpt =0;
 			File myObj = new File(fname);
 			Scanner myReader = new Scanner(myObj);
@@ -40,7 +44,9 @@ public class Graph
 				
 			}
 			myReader.close();
-	    } catch (Exception e) {
+
+	    } catch (Exception e) 
+	    {
 	    	System.out.println("Error");
 	    	e.printStackTrace();
 	    }
@@ -57,5 +63,37 @@ public class Graph
 		long number = Arrays.stream(all).distinct().count();
 		System.out.println("distinct count : "+ number);
 		return number;
+
+	}
+
+
+	public void build_adjacency_list(){
+		adja = new int[nbEdges + 1][nbEdges + 1];
+
+		for(int i = 0 ; i < from.length ; i ++ )
+		{
+			int edge_1 = from[i];
+			int edge_2 = to[i];
+
+			adja[edge_1][edge_2] = 1; 
+			adja[edge_2][edge_1] = 1;  
+		}
+	}
+
+	public void print_adjacency_list()
+	{
+		for(int i = 0 ; i < adja.length ; i ++)
+		{	
+			System.out.print("La liste d'adjacence pour le sommet n°" + i + ":" );
+
+			for(int j = 0 ; j < adja[i].length; j++)
+			{
+				if ( adja[i][j] == 1)
+				{
+					System.out.print(" "+ j+ " ");
+				}
+			}
+			System.out.println();
+		}
 	}
 }
