bfd189ffe9506fa4f09eefbb7a53fbfe9ac2855c
Lahdir Mohamed Akram <akramdzbuisness@gmail.com>
2021-02-03 17:06:06 +0100

    corrected some results

---
+++ tp2/lunch.sh
@@ -0,0 +1,14 @@
+echo "#graph\texact"
+
+java TP2 diametre g.txt 6 1
+java TP2 diametre ../Archives/as20000102.txt 14000 1
+#java TP2 diametre ../Archives/as-caida20071105.txt 110000	0
+java TP2 diametre ../Archives/as-skitter.txt	12000000 	0
+java TP2 diametre ../Archives/roadNet-TX.txt	1922000		0
+java TP2 diametre ../Archives/roadNet-CA.txt	2800000		0
+java TP2 diametre ../Archives/CA-AstroPh.txt	200000		84424
+java TP2 diametre ../Archives/com-dblp.ungraph.txt	1050000		0
+java TP2 diametre ../Archives/Email-Enron.txt	183831		0
+#java TP2 diametre ../Archives/soc-pokec-relationships.txt	31000000 	1
+java TP2 diametre ../Archives/web-BerkStan.txt	7600595 	1
+#java TP2 diametre ../Archives/bigdeg.txt	2010000 	0
